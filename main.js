9846e1ff794fb55bdda6c89e4ab9b6d8dcf6f992
Colin R <csrichards.90@gmail.com>
2019-05-04 21:04:00 -0500

    Create HTML, css, js files. Create basic DOM and helper functions

---
+++ main.js
@@ -0,0 +1,29 @@
+let output = document.querySelector("#output");
+
+//Operate triggers when an operator is clicked
+//1. Operate takes in the current input and the selected operator
+//2. Calls add/subtract/multiply/divide
+//3. Calls equate
+function operate() {
+
+}
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function equate () {
+
+}
